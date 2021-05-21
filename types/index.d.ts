e33f122d58b89db89d7a8c02fc83d341072fe3dd
ryan <1184518756@qq.com>
2021-05-21 15:44:06 +0800

    add docs

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,16 +4,78 @@
  * @param methodName    {string|Symbol}
  * @param doHookThings  {doHookThings}
  * @param options       {HookOptions=}
+ * @return {void}
  */
 export function hook(parent: any, methodName: string | Symbol, doHookThings: doHookThings, options?: HookOptions | undefined): void;
-export function hookFunc(parent: any, methodName: any, type: any, callback: any, options?: {}): void;
-export function hookAfter(parent: any, methodName: any, after: any, options?: {}): void;
-export function hookBefore(parent: any, methodName: any, before: any, options?: {}): void;
-export function hookError(parent: any, methodName: any, onError: any, options?: {}): void;
-export function hookSurround(parent: any, methodName: any, surround: any, options?: {}): void;
-export function hookReplace(parent: any, methodName: any, replace: any, options?: {}): void;
-export function protectMethod(parent: any, methodName: any): void;
+/**
+ * Hook target function to replace, using func-js instance
+ * @param parent {object}                   Parent object
+ * @param methodName {string | symbol}      Target method name to replace
+ * @param type {string} func-js instance    method name: before, after, onError, surround and etc
+ * @param callback {function | object}      func-js instance method params
+ * @param options
+ * @return {void}
+ */
+export function hookFunc(parent: object, methodName: string | symbol, type: string, callback: Function | object, options?: {}): void;
+/**
+ * Hook target function to replace, using func-js instance, and using `after` method
+ * @param parent {object}                   Parent object
+ * @param methodName {string | symbol}      Target method name to replace
+ * @param after {function}                  func-js instance method params of `after`
+ * @param options
+ * @return {void}
+ */
+export function hookAfter(parent: object, methodName: string | symbol, after: Function, options?: {}): void;
+/**
+ * Hook target function to replace, using func-js instance, and using `before` method
+ * @param parent {object}                   Parent object
+ * @param methodName {string | symbol}      Target method name to replace
+ * @param before {function}                 func-js instance method params of `before`
+ * @param options
+ * @return {void}
+ */
+export function hookBefore(parent: object, methodName: string | symbol, before: Function, options?: {}): void;
+/**
+ * Hook target function to replace, using func-js instance, and using `onError` method
+ * @param parent {object}                   Parent object
+ * @param methodName {string | symbol}      Target method name to replace
+ * @param onError {function}                 func-js instance method params of `onError`
+ * @param options
+ * @return {void}
+ */
+export function hookError(parent: object, methodName: string | symbol, onError: Function, options?: {}): void;
+/**
+ * Hook target function to replace, using func-js instance, and using `surround` method
+ * @param parent {object}                   Parent object
+ * @param methodName {string | symbol}      Target method name to replace
+ * @param surround {function}                 func-js instance method params of `surround`
+ * @param options
+ * @return {void}
+ */
+export function hookSurround(parent: object, methodName: string | symbol, surround: Function, options?: {}): void;
+/**
+ * Hook target function to replace, not to using func-js instance
+ * @param parent {object}                   Parent object
+ * @param methodName {string | symbol}      Target method name to replace
+ * @param replace {function}                To return a new function to replace origin method
+ * @param options
+ * @return {void}
+ */
+export function hookReplace(parent: object, methodName: string | symbol, replace: Function, options?: {}): void;
+/**
+ * Protect method from modified
+ * @param parent {object}
+ * @param methodName {string}
+ */
+export function protectMethod(parent: object, methodName: string): void;
+/**
+ * Sync the toString method from origin to target
+ * @param from {any}
+ * @param to {any}
+ */
 export function syncToString(from: any, to: any): void;
+export const defineProperty: (o: any, p: PropertyKey, attributes: PropertyDescriptor & ThisType<any>) => any;
+export const defineProperties: (o: any, properties: PropertyDescriptorMap & ThisType<any>) => any;
 /**
  * How to hook method
  */
